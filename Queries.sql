9b5b2d2f10742bd539bc481cf4f4d3f01f8e637d
Sahil Behera <sahilbehera1012007@gmail.com>
2026-06-23 13:49:56 +0530

    Add address column and queries for borrowing data

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,9 @@
+--Suppose you want to add an address column to the members table.
 
 ALTER TABLE members
 ADD address VARCHAR(255);
+
+--Find books that are currently borrowed.
 
 SELECT title, author
 FROM books
@@ -10,3 +13,38 @@ WHERE book_id IN (
     WHERE status = 'Borrowed'
 );
 
+-- View to easily see current active borrowers
+CREATE VIEW current_borrowers AS 
+SELECT 
+  m.first_name, 
+  m.last_name, 
+  b.title, 
+  bt.borrow_date, 
+  bt.due_date 
+FROM members m 
+JOIN borrow_transactions bt ON m.member_id = bt.member_id 
+JOIN books b ON bt.book_id = b.book_id 
+WHERE bt.status = 'Borrowed' OR bt.status = 'Overdue';
+
+--Find members who have borrowed more than 2 books.
+
+SELECT 
+    member_id,
+    COUNT(*) AS books_borrowed
+FROM borrow_transactions
+GROUP BY member_id
+HAVING COUNT(*) > 2;
+
+--Display member names along with the books they have borrowed.
+
+SELECT 
+    m.first_name,
+    m.last_name,
+    b.title,
+    bt.borrow_date,
+    bt.due_date
+FROM members m
+INNER JOIN borrow_transactions bt
+    ON m.member_id = bt.member_id
+INNER JOIN books b
+    ON bt.book_id = b.book_id;
