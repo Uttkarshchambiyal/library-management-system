d4eeaf4e4ccf17dbbe0cbe89b5e2774047b3c974
Sahil Behera <sahilbehera1012007@gmail.com>
2026-06-23 13:43:05 +0530

    Alter members table and add borrowed books query
    Added address column to members table and a query to select borrowed books.

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,12 @@
 
+ALTER TABLE members
+ADD address VARCHAR(255);
+
+SELECT title, author
+FROM books
+WHERE book_id IN (
+    SELECT book_id
+    FROM borrow_transactions
+    WHERE status = 'Borrowed'
+);
+
